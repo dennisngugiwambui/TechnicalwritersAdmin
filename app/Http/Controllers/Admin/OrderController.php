a58ec97c229d6444e522ea2f66ec6fb35fe34025
dennisngugiwambui <dellyit001@gmail.com>
2025-04-11 18:45:54 +0300

    new commit added

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -1169,7 +1169,7 @@ class OrderController extends Controller
          *
          * @param  \Illuminate\Http\Request  $request
          * @return \Illuminate\Http\JsonResponse
-         * @return \Illuminate\
+         * @return \
          */
         public function getStatistics(Request $request)
         {
