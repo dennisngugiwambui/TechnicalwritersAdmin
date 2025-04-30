cf239ba7d9fe13bad769c8a312af0bc5aa462db8
dennisngugiwambui <dellyit001@gmail.com>
2025-04-30 21:18:18 +0300

    new commit added

--- routes/web.php
+++ routes/web.php
@@ -47,7 +47,7 @@ Route::get('/admin/dashboard/chart-data', [AdminHomeController::class, 'getChart
     ->name('admin.dashboard.chart-data')
     ->middleware('auth');
 
-// Admin routes
+// Admin routes a
 Route::prefix('admin')->middleware(['auth'])->name('admin.')->group(function () {
     // Dashboard
     Route::get('/', [AdminHomeController::class, 'index'])->name('dashboard');
